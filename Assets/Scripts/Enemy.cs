809974f34b22def35a849ffec052a29c039e8238
GAmuzak <thatamuzak@gmail.com>
2021-08-22 02:31:14 +0530

    yee 3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float enemyHealth;
     
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            ReduceHealth();
+        }
+    }
+
+    private void ReduceHealth()
+    {
+        throw new NotImplementedException();
+    }
 }
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,24 +1,49 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
     [SerializeField] private float range;
     [SerializeField] private LayerMask layerMask;
+    [Header("Shooting Stuff")]
+    [SerializeField] private float fireRate;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletContainer;
+    [SerializeField] private float bulletSpeed;
 
+
+    private Transform leftGun;
+    private Transform rightGun;
     private GameObject target = null;
+    private bool canShoot=true;
 
     private void Start()
-    {   
-        InvokeRepeating(nameof(FindClosestEnemy), 0f, 0.1f);
+    {
+        leftGun = transform.GetChild(1);
+        rightGun = transform.GetChild(2);
+        InvokeRepeating(nameof(FindClosestEnemy), 0f, 0.5f);
     }
 
     private void Update()
     {
         if (target == null) return;
         Vector3 dirn = target.transform.position - transform.position;
-        Vector3 rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dirn), Time.deltaTime*10f).eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dirn), Time.deltaTime*30f).eulerAngles;
         transform.rotation=Quaternion.Euler(0f, rotation.y, 0f);
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (!canShoot) return;
+        GameObject leftBullet=Instantiate(bullet, leftGun.position, Quaternion.identity, bulletContainer);
+        GameObject rightBullet=Instantiate(bullet, rightGun.position, Quaternion.identity, bulletContainer);
+        Vector3 forward = transform.forward;
+        leftBullet.GetComponent<Rigidbody>().AddForce(bulletSpeed*forward);
+        rightBullet.GetComponent<Rigidbody>().AddForce(bulletSpeed*forward);
+        canShoot = false;
+        StartCoroutine(ShootCoolDown());
     }
 
     private void FindClosestEnemy()
@@ -41,6 +66,12 @@ public class WeaponController : MonoBehaviour
             closestTarget = enemy.gameObject;
         }
         target = shortestDistance <= range ? closestTarget : null;
+    }
+
+    private IEnumerator ShootCoolDown()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
     }
 
     private void OnDrawGizmosSelected()
