d98a196396636991d005025d9bd3c2b35945b8a1
GAmuzak <thatamuzak@gmail.com>
2021-08-21 20:20:11 +0530

    yee 2

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI healthTextBox;
+    [Range(50, 150)][SerializeField] private int startingHealth;
+
+    private int health = 0;
+    private void Start()
+    {
+        healthTextBox.text = startingHealth.ToString();
+        health = startingHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            ReduceHealth();
+        }
+    }
+
+    private void ReduceHealth()
+    {
+        health--;
+        healthTextBox.text = health.ToString();
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,5 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("Finish"))
-            Destroy(gameObject);
-    }
+    
 }
